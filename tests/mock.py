22497d64573367f9d6668c382261a34f0cc9f614
abdellatif yaakoubi <abdellatif yaakoubi.1@ens.etsmtl.ca>
2020-07-08 20:31:39 -0400

    update ignore file1

--- tests/mock.py
+++ tests/mock.py
@@ -1,4 +1,10 @@
 from mock import MagicMock
+
+
+class ProductionClass(object):
+    pass
+
+
 thing = ProductionClass()
 thing.method = MagicMock(return_value=3)
 thing.method(3, 4, 5, key='value')
