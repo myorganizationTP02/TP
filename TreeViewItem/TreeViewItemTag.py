5f5f2a3eb3d43fc290c2f7c918b140bd8485a377
abdellatif yaakoubi <abdellatif yaakoubi.1@ens.etsmtl.ca>
2020-07-12 13:07:29 -0400

    visitscreen

--- TreeViewItem/TreeViewItemTag.py
+++ TreeViewItem/TreeViewItemTag.py
@@ -4,13 +4,14 @@ from generalElements.popups.InputPopup import InputPopup
 from generalElements.popups.NormalPopup import NormalPopup
 from generalElements.popups.ConfirmPopup import ConfirmPopup
 
+
 class TreeViewItemTag(TreeViewItem):
     indent = 1
     can_rename_folder = True
     can_delete_folder = True
+    screenDatabase = ''
 
-
-    def visit(self,screenDatabase=[]):
+    def visit(self, screenDatabase):
         super(TreeViewItemTag, self).visit()
 
         screenDatabase = self.owner
@@ -22,12 +23,11 @@ class TreeViewItemTag(TreeViewItem):
         screenDatabase.update_can_browse()
         screenDatabase.update_selected()
 
-    def visit_drop(self,visitors):
-        self.owner.add_to_tag(self.item,visitors)
+    def visit_drop(self, visitors):
+        self.owner.add_to_tag(self.item, visitors)
         self.target = 'allo'
 
-
-    def rename_item(self,photoListRecyclerView):
+    def rename_item(self, photoListRecyclerView):
         """Starts the folder renaming process, creates an input text popup."""
 
         content = InputPopup(hint=self.item.name, text='Rename To:')
@@ -49,18 +49,16 @@ class TreeViewItemTag(TreeViewItem):
         if answer == 'yes':
             text = instance.ids['input'].text.strip(' ')
             app = App.get_running_app()
-            self.item.name =text
+            self.item.name = text
             app.session.commit()
 
         self.popup.dismiss()
         self.__refresh_tree_view()
 
-
-    def delete_item(self,photoListRecyclerView):
+    def delete_item(self, photoListRecyclerView):
         """Starts the delete folder process, creates the confirmation popup."""
 
-
-        text = "Delete tag "+ self.item.name + "\nThe Contained Files Will Not Be Deleted."
+        text = "Delete tag " + self.item.name + "\nThe Contained Files Will Not Be Deleted."
         content = ConfirmPopup(text=text, yes_text='Delete', no_text="Don't Delete", warn_yes=True)
         app = App.get_running_app()
         self.photoListRecyclerView = photoListRecyclerView
@@ -78,10 +76,9 @@ class TreeViewItemTag(TreeViewItem):
         del instance
         if answer == 'yes':
             self.item.delete()
-            #self.previous_album()
+            # self.previous_album()
         self.popup.dismiss()
         self.__refresh_tree_view()
-
 
     def __refresh_tree_view(self):
         if self.treeViewItemParent.expanded:
